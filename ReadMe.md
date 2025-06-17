b29b3a5ae87bca72b70f4cc01e7fe517d9c62fec
mahan <prabhat.nist4@gmail.com>
2025-06-17 22:48:21 +0530

    first commit

---
+++ ReadMe.md
@@ -0,0 +1,10 @@
+1. Create necessary AWS components.
+2. Code GET and POST endpoints.
+3. Dockerize flask app.
+4. Push flask app to Github.
+5. Create CI/CD using Jenkins.
+6. Access app via Route 53 domain.
+7. Connect flask app to AWS RDS.
+8. Store private values in AWS secret manager.
+9. Schedule Stop/Start of EC2 instance.
+10. Automate alerts if ECS instance fails.
---
+++ api.py
@@ -0,0 +1,36 @@
+from flask import Flask
+from flask_restful import Resource, Api, reqparse, abort
+
+app = Flask(__name__)
+api = Api(app)
+
+todos = {
+    1: {"task":"Write Hello World Program", "Summary":"Write the code using Python"},
+    2: {"task":"Task 2", "Summary":"Writing task 2"},
+    3: {"task":"Task 3", "Summary":"this is task 3"}
+}
+
+task_post_args = reqparse.RequestParser()
+task_post_args.add_argument("task", type=str, help="Task description is required", required=True)
+task_post_args.add_argument("summary", type=str, help="Task summary is required", required=True)
+
+class ToDoList(Resource):
+  def get(self):
+      return todos
+  
+class ToDo(Resource):
+  def get(self, todo_id):
+      return todos[todo_id]
+  
+  def post(self, todo_id):
+      args = task_post_args.parse_args()
+      if todo_id in todos:
+         abort(409, "Task Id already taken")
+      todos[todo_id] = {"task": args["task"], "summary": args["summary"]}
+      return todos[todo_id], 201
+
+api.add_resource(ToDo, '/todos/<int:todo_id>')
+api.add_resource(ToDoList, '/todos')
+
+if __name__ == '__main__':
+  app.run(debug=True)
